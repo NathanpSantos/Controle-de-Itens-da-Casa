e2fb347a9dd4df137cff492888d35d4e3aba3b8e
Nathan Santos <nathanprsantos@outlook.com>
2024-07-10 18:45:46 -0300

    Bind Flask app to the correct port for Heroku

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -119,4 +120,5 @@ def delete_cleaning_product(product_id):
     return redirect(url_for('cleaning_products'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
